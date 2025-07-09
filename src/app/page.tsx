aba7992a84634dfa23cd138cc9feb66520aa74f7
JunYuen <junyuenchong1998@gmail.com>
2025-07-09 14:38:00 +0800

    fix .env error 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,12 @@ import { getAllCategories } from '@/actions/category';
 import React from 'react';
 
 const Home = async () => {
-  const { user } = await getCurrentSession();
-
-  const categories = await getAllCategories();
-
   return (
     <div>
-      {/* {JSON.stringify(user)}
-   
-      {JSON.stringify(categories)} */}
       <p>Hello</p>
     </div>
   );
 };
+
 
 export default Home;
