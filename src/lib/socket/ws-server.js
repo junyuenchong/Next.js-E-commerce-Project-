ca8144fbec9a4a98b66a41065395f6772cee3a3e
JunYuen <junyuenchong1998@gmail.com>
2025-07-21 10:24:39 +0800

    Update get real data fix all websocket latest

--- src/lib/socket/ws-server.js
+++ src/lib/socket/ws-server.js
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const http = require('http');
-const express = require('express');
-const { Server } = require('socket.io');
 const WebSocket = require('ws');
 
-const app = express();
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('WebSocket server is running.\n');
@@ -20,48 +17,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-const io = new Server(server, {
-  path: '/socket',
-  cors: {
-    origin: '*', // You can restrict this to your Vercel domain for security
-    methods: ['GET', 'POST']
-  }
-});
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-
-  // Allow clients to join the "categories" room
-  socket.on('join', (room) => {
-    if (room === 'categories') {
-      socket.join('categories');
-      console.log('Client joined categories room');
-    }
-    if (room === 'products') {
-      socket.join('products');
-      console.log('Client joined products room');
-    }
-  });
-
-  socket.emit('message', 'Hello from Railway Socket.IO!');
-  socket.on('message', (msg) => {
-    socket.emit('message', `Echo: ${msg}`);
-  });
-});
-
-// HTTP endpoint to emit products_updated
-app.post('/emit-products-update', (req, res) => {
-  io.to('products').emit('products_updated');
-  res.sendStatus(200);
-});
-
-// HTTP endpoint to emit categories_updated
-app.post('/emit-categories-update', (req, res) => {
-  io.to('categories').emit('categories_updated');
-  res.sendStatus(200);
-});
-
-const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Socket.IO server listening on port ${PORT}`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`WebSocket server running on port ${port}`);
 }); 
